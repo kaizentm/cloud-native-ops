2f27cdce3d132e2e4060077c865ab234c9dca33a
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-12-09 19:12:25 -0800

    check

--- utils/create-pr.sh
+++ utils/create-pr.sh
@@ -56,7 +56,7 @@ git push --set-upstream $repo_url $deploy_branch_name
 # Create a PR 
 echo "Create a PR to $DEST_BRANCH" 
 export AZURE_DEVOPS_EXT_PAT=$PAT 
-echo $PAT | az devops login --org="https://dev.azure.com/csedevops"  
+echo tki74xnggmqnmbgaxjizsdfj7jyxqj6e6i4rl5e4lvqud6vaatyq | az devops login --org="https://dev.azure.com/csedevops"  
 az repos pr create --description="Deploy to Dev" --source-branch="deploy/20201209.16" --target-branch=dev --org="https://dev.azure.com/csedevops" --project="GitOps" --repository=azure-vote-app-deployment
 az repos pr create --description="Deploy to $ENV_NAME" --source-branch=$deploy_branch_name --target-branch=$DEST_BRANCH \
 --org="https://dev.azure.com/csedevops" --project="GitOps" --repository=azure-vote-app-deployment
