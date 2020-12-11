2a4486d15d16f5b6fd8832aafc58fae7f8824a8e
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-12-10 17:45:39 -0800

    sh

--- utils/create-pr.sh
+++ utils/create-pr.sh
@@ -54,25 +54,9 @@ echo "git push --set-upstream $repo_url $deploy_branch_name"
 git push --set-upstream $repo_url $deploy_branch_name
 
 # Create a PR 
-# Old version of Azure CLI
-# Just Rest API?
 echo "Create a PR to $DEST_BRANCH" 
-# az upgrade --all -y
-# export AZURE_DEVOPS_EXT_PAT=$PAT 
-# echo tki74xnggmqnmbgaxjizsdfj7jyxqj6e6i4rl5e4lvqud6vaatyq | az devops login --organization https://dev.azure.com/csedevops
-# az repos pr create --description="Deploy to Dev" --source-branch="deploy/20201209.16" --target-branch=dev --org="https://dev.azure.com/csedevops" --project="GitOps" --repository=azure-vote-app-deployment
-# az repos pr create --description="Deploy to $ENV_NAME" --source-branch=$deploy_branch_name --target-branch=$DEST_BRANCH \
-# --org="https://dev.azure.com/csedevops" --project="GitOps" --repository=azure-vote-app-deployment
-
-# curl -v -H "Authorization: Basic $B64_PAT" -H "Content-Type: application/json"\
-#         -d '{"sourceRefName":"refs/heads/deploy/20201209.16", "targetRefName":"refs/heads/dev", "description":"Deploy to Dev", "title":"Deploy to Dev"}' \
-#        'https://dev.azure.com/csedevops/GitOps/_apis/git/repositories/6b37c26c-06e2-45ae-8076-7c37aa34510d/pullrequests?api-version=6.1-preview.1'       
-
-# curl -v -H "Authorization: Basic $B64_PAT" -H "Content-Type: application/json"\
-#         -d '{"sourceRefName":"refs/heads/deploy/20201209.22", "targetRefName":"refs/heads/dev", "description":"Deploy to Dev", "title":"Deploy to Dev"}' \
-#        'https://dev.azure.com/csedevops/GitOps/_apis/git/repositories/azure-vote-app-deployment/pullrequests?api-version=6.1-preview.1'              
 
 B64_PAT=$(printf ":$TOKEN" | base64)
 curl -v -H "Authorization: Basic $B64_PAT" -H "Content-Type: application/json" --fail \
-        -d '{"sourceRefName":"refs/heads/'$deploy_branch_name'", "targetRefName":"refs/heads/'$DEST_BRANCH'", "description":"Deploy to $ENV_NAME", "title":"deployment $DEPLOY_ID"}' \
+        -d '{"sourceRefName":"refs/heads/'$deploy_branch_name'", "targetRefName":"refs/heads/'$DEST_BRANCH'", "description":"Deploy to '$ENV_NAME'", "title":"deployment '$DEPLOY_ID'"}' \
        "$SYSTEM_COLLECTIONURI$SYSTEM_TEAMPROJECT/_apis/git/repositories/$repo_name/pullrequests?api-version=6.1-preview.1"      
