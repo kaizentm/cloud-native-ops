8d1d064f83ebf3ae2eae3f66209b4c75727a6eb2
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-12-13 17:00:11 -0800

    pr flow

--- gitops-events/gitops_event_handler.py
+++ gitops-events/gitops_event_handler.py
@@ -4,6 +4,7 @@ from azdo_gitops import AzureDevOpsGitOps
 
 application = Flask(__name__)
 
+temp_data = {}
 
 @application.route("/gitopsphase/", methods=['POST'])
 def gitopsphase():
@@ -12,10 +13,20 @@ def gitopsphase():
     print(f'GitOps phase: {payload}')
     
     azdo_gitops = AzureDevOpsGitOps()
-    azdo_gitops.process_gitops_phase(payload)
+    azdo_gitops.process_gitops_phase(payload, temp_data)
 
     return f'GitOps phase: {payload}', 200    
 
+@application.route("/deploy", methods=['POST'])
+def deploy():
+    payload = request.get_json()
+
+    print(f'deploy: {payload}')
+    
+    global temp_data
+    temp_data = payload
+
+    return f'deploy: {payload}', 200    
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
