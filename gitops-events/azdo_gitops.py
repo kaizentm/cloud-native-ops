ce9f231fc11379a9a5db60ecaf08df9c8b474842
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-12-14 18:54:20 -0800

    GitOps with Azure DevOps and ArgoCD

--- gitops-events/azdo_gitops.py
+++ gitops-events/azdo_gitops.py
@@ -15,39 +15,22 @@ class AzureDevOpsGitOps:
         self.headers = {'authorization': f'Basic {self.token}',
                         'Content-Type' : 'application/json'}
 
-    def process_gitops_phase(self, gitops_phase_data, pr_task_data):
+    def process_gitops_phase(self, gitops_phase_data):
         print(gitops_phase_data)
         commitid = gitops_phase_data['commitid']
         state = gitops_phase_data['state']
         health = gitops_phase_data['health']
         message = gitops_phase_data['message']
+        self.update_commit_status(commitid, state, health, message)
+
+    def update_commit_status(self, commitid, state, health, message):
+        url = self.org_url + f'/_apis/git/repositories/azure-vote-app-deployment/commits/{commitid}/statuses?api-version=6.0'
         if state=="Running" or health=="Progressing":
             state = "Pending"   
         if health=="Degraded":
             state = "Failed"             
-
-        self.update_commit_status(commitid, state, health, message)
-
-        if state == "Succeeded" or state == "Failed" or state == "Error":
-            if state == "Error":
-                state = "Failed"
-            self.update_pr_task(state, pr_task_data)
-
-    def update_commit_status(self, commitid, state, health, message):
-        url = self.org_url + f'/_apis/git/repositories/azure-vote-app-deployment/commits/{commitid}/statuses?api-version=6.0'
         callback_url = os.getenv("GITOPS_APP_URL") 
         data = {'state': state, 'description': message, 'targetUrl': callback_url, 'context': {'name': health, 'genre': 'Health'} }
         response = requests.post(url=url, headers=self.headers, json=data)
         print(response.content)
         assert response.status_code == 201
-
-
-    def update_pr_task(self, state, pr_task_data):
-        planurl = pr_task_data['planurl']
-        projectid = pr_task_data['projectid']
-        planid = pr_task_data['planid']
-        url = f'{planurl}{projectid}/_apis/distributedtask/hubs/build/plans/{planid}/events?api-version=2.0-preview.1'
-        data = {'name': "TaskCompleted", 'taskId': pr_task_data['taskid'], 'jobid': pr_task_data['jobid'], 'result': state }
-        response = requests.post(url=url, headers=self.headers, json=data)
-        print(response.content)
-        assert response.status_code == 204
--- gitops-events/gitops_event_handler.py
+++ gitops-events/gitops_event_handler.py
@@ -4,7 +4,6 @@ from azdo_gitops import AzureDevOpsGitOps
 
 application = Flask(__name__)
 
-temp_data = {}
 
 @application.route("/gitopsphase/", methods=['POST'])
 def gitopsphase():
@@ -13,22 +12,10 @@ def gitopsphase():
     print(f'GitOps phase: {payload}')
     
     azdo_gitops = AzureDevOpsGitOps()
-    azdo_gitops.process_gitops_phase(payload, temp_data)
+    azdo_gitops.process_gitops_phase(payload)
 
     return f'GitOps phase: {payload}', 200    
 
-@application.route("/deploy", methods=['POST'])
-def deploy():
-    payload = request.get_json()    
-
-    print(f'deploy: {payload}')
-    
-    global temp_data
-    temp_data = payload
-
-    return f'deploy: {payload}', 200    
-
-# Add an azdo webhook listener to Invoke argo cd sync on push
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
