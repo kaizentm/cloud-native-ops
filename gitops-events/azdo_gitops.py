d9851778fd88f8b4b5fc02efcdee5e6cd108ba50
Tom Care <tcare@microsoft.com>
2021-01-08 00:13:07 +0000

    Merged PR 863: GitOps connector cleanup + hardening
    - General cleanup
    - Store the callback task metadata in AzDO PR metadata, but keep the in-memory cache and evict after callback
    - Requests emit exceptions based on HTTP codes
    - Basic logging control and status messages
    - Handle basic edge cases gracefully e.g. task timeout

--- gitops-events/azdo_gitops.py
+++ gitops-events/azdo_gitops.py
@@ -1,11 +1,13 @@
+import base64
+import json
+import logging
 import os
 import requests
-import json
-import base64
 
-# Temp storage in memory for a prototype. 
-# To be changed. The connection between the PR and agentless task should be stored in the PR itself
-pr_task_data = {}
+# We need to store the agentless task ID that is waiting for a PR to complete.
+# The data is stored remotely in the PR metadata, and we also cache it locally.
+PR_METADATA_KEY = "argocd-callback-task-id"
+pr_task_data_cache = {}
 
 
 class AzureDevOpsGitOps:
@@ -17,8 +19,9 @@ class AzureDevOpsGitOps:
 
     def __init__(self):
         self.org_url = os.getenv("AZDO_ORG_URL")  #https://dev.azure.com/csedevops/GitOps
+        self.gitops_repo_name = os.getenv("AZDO_GITOPS_REPO_NAME") #
         # token is supposed to be stored in a secret without any transformations
-        self.token = base64.b64encode(f':{os.getenv("PAT")}'.encode("ascii")).decode("ascii")  
+        self.token = base64.b64encode(f':{os.getenv("PAT")}'.encode("ascii")).decode("ascii")
         self.headers = {'authorization': f'Basic {self.token}',
                         'Content-Type' : 'application/json'}
 
@@ -53,16 +56,6 @@ class AzureDevOpsGitOps:
         }
         return health_map[health]
 
-    def process_gitops_phase(self, gitops_phase_data):
-        print(gitops_phase_data)
-        commitid = gitops_phase_data['commitid']
-        phase = gitops_phase_data['phase']
-        sync_status = gitops_phase_data['sync_status']
-        health = gitops_phase_data['health']
-        message = gitops_phase_data['message']
-        resources = gitops_phase_data['resources']
-        self.update_commit_statuses(commitid, phase, sync_status, health, message, resources)
-
     def get_deployment_status_summary(self, resources):
         total = len(resources)
         health_count = {}
@@ -88,84 +81,180 @@ class AzureDevOpsGitOps:
         return (summarize(health_count), summarize(sync_count))
 
     def update_commit_status(self, commitid, status_name, azdo_state, message):
-        url = self.org_url + f'/_apis/git/repositories/azure-vote-app-deployment/commits/{commitid}/statuses?api-version=6.0'
+        url = f'{self.org_url}/_apis/git/repositories/{self.gitops_repo_name}/commits/{commitid}/statuses?api-version=6.0'
 
         callback_url = os.getenv("GITOPS_APP_URL")
         data = {'state': azdo_state, 'description': status_name + ": " + message, 'targetUrl': callback_url + "?noop=" + status_name, 'context': {'name': status_name, 'genre': 'ArgoCD'} }
         response = requests.post(url=url, headers=self.headers, json=data)
-        print(response.content)
-        print(response.status_code)
-        assert response.status_code == 201
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
 
     def get_pr_num(self, commitid):
-        url = self.org_url + f'/_apis/git/repositories/azure-vote-app-deployment/commits/{commitid}?api-version=6.0'
+        url = f'{self.org_url}/_apis/git/repositories/{self.gitops_repo_name}/commits/{commitid}?api-version=6.0'
+
         response = requests.get(url=url, headers=self.headers)
-        commit = json.loads(response.content)                
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
+
+        commit = response.json()
         comment = commit['comment']
         MERGED_PR="Merged PR "
         pr_num = None
         if MERGED_PR in comment:
             pr_num = comment[comment.index(MERGED_PR) + len(MERGED_PR) : comment.index(":")]
         return pr_num
-    
-    # These too functions update_pr_task_data and get_pr_task_data will be changed 
-    # when we change how pr_num<->taskid connection is stored
-    def update_pr_task_data(self, payload):
-        global pr_task_data
-        pr_task_data[payload['pr_num']] = payload
 
-    def get_pr_task_data(self, pr_num):        
-        if pr_num in pr_task_data:
-            return pr_task_data[pr_num]
-        else:
-            return None
+    def get_pr_metadata(self, pr_num, key):
+        # https://docs.microsoft.com/en-us/rest/api/azure/devops/git/pull%20request%20properties/list?view=azure-devops-rest-6.0
+        url = f'{self.org_url}/_apis/git/repositories/{self.gitops_repo_name}/pullRequests/{pr_num}/properties?api-version=6.0-preview'
 
+        response = requests.get(url=url, headers=self.headers)
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
+
+        # Navigate the properties response structure
+        result = response.json()
+        if (result['count'] > 0):
+            properties = result['value']
+            entry = properties.get(key)
+            if entry:
+                # At this point, we have the original JSON string we stored.
+                return json.loads(entry['$value'])
+
+        return None
+
+    ### Will fail if metadata already exists for the given path.
+    def add_pr_metadata(self, pr_num, key, value):
+        # https://docs.microsoft.com/en-us/rest/api/azure/devops/git/pull%20request%20properties/update?view=azure-devops-rest-6.0
+        url = f'{self.org_url}/_apis/git/repositories/{self.gitops_repo_name}/pullRequests/{pr_num}/properties?api-version=6.0-preview'
+        value_str = json.dumps(value)
+        escaped_str = json.dumps(value_str)
+
+        # Key must start with '/', even though it won't be returned as a path in GET
+        payload = f"""
+            [
+                {{
+                    "op": "add",
+                    "path": "/{key}",
+                    "from": null,
+                    "value": {escaped_str}
+                }}
+            ]
+        """
+
+        headers_patch = self.headers.copy()
+        headers_patch['Content-Type'] = "application/json-patch+json"
+
+        response = requests.patch(url=url, headers=headers_patch, data=payload)
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
+
+    # Read/write for PR->TaskID value in AzDO and local cache.
+    def add_pr_task_data(self, payload):
+        logging.info(f'PR {payload["pr_num"]}: Storing and caching metadata for task {payload["taskid"]}')
+        pr_task_data_cache[payload['pr_num']] = payload
+
+        self.add_pr_metadata(payload['pr_num'], PR_METADATA_KEY, payload)
+
+    def get_pr_task_data(self, pr_num):
+        if pr_num in pr_task_data_cache:
+            logging.info(f'PR {pr_num}: Metadata read cache hit')
+            # We only expect to have one callback, so remove the cache entry.
+            return pr_task_data_cache.pop(pr_num)
+
+        logging.warning(f'PR {pr_num}: Metadata cache miss')
+        return self.get_pr_metadata(pr_num, PR_METADATA_KEY)
+
+    # Given a PR task, check if it's parent plan has already completed.
+    # Note: Completed does not necessarily mean it succeeded.
+    def plan_already_completed(self, pr_task):
+        planurl = pr_task['planurl']
+        projectid = pr_task['projectid']
+        planid = pr_task['planid']
+        url = f'{planurl}{projectid}/_apis/distributedtask/hubs/build/plans/{planid}'
+
+        response = requests.get(url=url, headers=self.headers)
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
+
+        plan_info = response.json()
+        return plan_info['state'] == 'completed'
+
+    # Update the Azure Pipeline task waiting for the PR to complete.
     def update_pr_task(self, state, pr_num):
+        logging.info(f'PR {pr_num}: Rollout {state}, attempting task completion callback...')
         pr_task = self.get_pr_task_data(pr_num)
-        if pr_task:
-            planurl = pr_task['planurl']
-            projectid = pr_task['projectid']
-            planid = pr_task['planid']
-            url = f'{planurl}{projectid}/_apis/distributedtask/hubs/build/plans/{planid}/events?api-version=2.0-preview.1'
-            data = {'name': "TaskCompleted", 'taskId': pr_task['taskid'], 'jobid': pr_task['jobid'], 'result': state }
-            response = requests.post(url=url, headers=self.headers, json=data)
-            print(response.content)
-        else:
-            print(f'There is no data for pr [{pr_num}]')
+        if not pr_task:
+            logging.error(f'PR {pr_num} has no metadata! Cannot complete task callback.')
+            return
 
-    def update_commit_statuses(self, commitid, phase, sync_status, health, message, resources):
-        phase_status = self.map_phase_to_azdo_status(phase)
-        self.update_commit_status(commitid, 'Phase', phase_status, phase + ": " + message)
+        # The build task may have been cancelled, timed out, etc.
+        # Working with the plan in this state can cause 500 errors.
+        # Finish gracefully so ArgoCD doesn't keep calling us.
+        if self.plan_already_completed(pr_task):
+            return
 
-        (health_summary, sync_summary) = self.get_deployment_status_summary(resources)
+        planurl = pr_task['planurl']
+        projectid = pr_task['projectid']
+        planid = pr_task['planid']
+        url = f'{planurl}{projectid}/_apis/distributedtask/hubs/build/plans/{planid}/events?api-version=2.0-preview.1'
+        data = {
+            'name': "TaskCompleted",
+            'taskId': pr_task['taskid'],
+            'jobid': pr_task['jobid'],
+            'result': state
+        }
+        response = requests.post(url=url, headers=self.headers, json=data)
+        # Throw appropriate exception if request failed
+        response.raise_for_status()
 
-        self.update_commit_status(commitid, 'Sync', self.map_sync_status_to_azdo_status(sync_status), sync_summary)
+        logging.info(f'PR {pr_num}: Successfully completed task {pr_task["taskid"]}')
 
-        health_status = self.map_health_to_azdo_status(health)
-        self.update_commit_status(commitid, 'Health', health_status, health_summary)
-                
-        if phase_status != 'pending' and health_status != 'pending':  
+    # Update the statuses in Azure DevOps in the commit history view.
+    def update_commit_statuses(self, phase_data):
+        logging.info(f'Commit {phase_data["commitid"]}: Updating phase data')
+        phase_status = self.map_phase_to_azdo_status(phase_data['phase'])
+        self.update_commit_status(phase_data['commitid'], 'Phase', phase_status, phase_data['phase'] + ": " + phase_data['message'])
+
+        (health_summary, sync_summary) = self.get_deployment_status_summary(phase_data['resources'])
+
+        self.update_commit_status(phase_data['commitid'], 'Sync', self.map_sync_status_to_azdo_status(phase_data['sync_status']), sync_summary)
+
+        health_status = self.map_health_to_azdo_status(phase_data['health'])
+        self.update_commit_status(phase_data['commitid'], 'Health', health_status, health_summary)
+
+        logging.info(f'Commit {phase_data["commitid"]}: Updating phase data')
+
+        if phase_status != 'pending' and health_status != 'pending':
             if phase_status == 'succeeded' and health_status == 'succeeded':
-               state = 'succeeded'
+                state = 'succeeded'
             else:
                 state = 'failed'
-            pr_num = self.get_pr_num(commitid)
+            pr_num = self.get_pr_num(phase_data['commitid'])
             if pr_num:
                 self.update_pr_task(state, pr_num)
 
     def get_pull_request(self, pr_num):
-        url = self.org_url + f'/_apis/git/pullrequests/{pr_num}?api-version=6.1-preview.1'
+        url = f'{self.org_url}/_apis/git/pullrequests/{pr_num}?api-version=6.1-preview.1'
         response = requests.get(url=url, headers=self.headers)
-        pr = json.loads(response.content)                
+        pr = json.loads(response.content)
         return pr
-    
-    
-    def process_pr_status_updated(self, payload):
-        pr_num = payload['resource']['pullRequestId']
-        print(f'pr_num:[{pr_num}]')
+
+    def ensure_pr_alive(self, pr_num):
         pr = self.get_pull_request(pr_num)
         pr_status = pr['status']
         if (pr_status == 'abandoned'):
             self.update_pr_task('failed', str(pr_num))
+            return False
+        return True
 
+    def process_pr_status_updated(self, payload):
+        pr_num = payload['resource']['pullRequestId']
+        self.ensure_pr_alive(pr_num)
 
+    def process_update_pr_task(self, payload):
+        # Before checking if we should add PR data, make sure we're in a good state
+        if not self.ensure_pr_alive(payload["pr_num"]):
+            return
+
+        self.add_pr_task_data(payload)
--- gitops-events/gitops_event_handler.py
+++ gitops-events/gitops_event_handler.py
@@ -1,44 +1,42 @@
 from flask import Flask, request
 from azdo_gitops import AzureDevOpsGitOps
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 application = Flask(__name__)
 
+azdo_gitops = AzureDevOpsGitOps()
 
 @application.route("/gitopsphase/", methods=['POST'])
 def gitopsphase():
     payload = request.get_json()
 
-    print(f'GitOps phase: {payload}')
-    
-    azdo_gitops = AzureDevOpsGitOps()
-    azdo_gitops.process_gitops_phase(payload)
+    logging.debug(f'GitOps phase: {payload}')
 
-    return f'GitOps phase: {payload}', 200    
+    azdo_gitops.update_commit_statuses(payload)
+
+    return f'GitOps phase: {payload}', 200
 
 @application.route("/update-pr-task", methods=['POST'])
 def update_pr_task():
-    payload = request.get_json()    
+    payload = request.get_json()
 
-    print(f'update-pr-task: {payload}')
-    
-    azdo_gitops = AzureDevOpsGitOps()
-    
-    azdo_gitops.update_pr_task_data(payload)
-    
-    return f'update-pr-task: {payload}', 200    
+    logging.debug(f'update-pr-task: {payload}')
+
+    azdo_gitops.process_update_pr_task(payload)
+
+    return f'update-pr-task: {payload}', 200
 
 @application.route("/pr-status-updated", methods=['POST'])
 def pr_status_updated():
-    payload = request.get_json()    
+    payload = request.get_json()
 
-    print(f'pr_status_updated: {payload}')
-    
-    azdo_gitops = AzureDevOpsGitOps()
-    
-    azdo_gitops.process_pr_status_updated(payload)    
-    
-    return f'pr_status_updated: {payload}', 200    
+    logging.debug(f'pr_status_updated: {payload}')
+
+    azdo_gitops.process_pr_status_updated(payload)
+
+    return f'pr_status_updated: {payload}', 200
 
 # Add an azdo webhook listener to Invoke argo cd sync on push
 
